1833cd048fa170c328fa1af3422f139345807efe
Marko Stankovic <marko.stankovic@boohoo.com>
2022-06-24 12:46:56 +0200

    asm 04 project completed

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,56 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+
+(LOOP_KBD)
+    @SCREEN
+    D=A
+    @offset
+    M=0
+    @KBD
+    D=M
+    @BLACK
+    D;JNE
+    @WHITE
+    D;JEQ
+@LOOP_KBD
+0;JMP
+
+(BLACK)
+    @offset
+    D=M
+    @8191
+    D=D-A //n-R1
+    @LOOP_KBD
+    D;JGT //n-R1<0 continue in loop else jump to end
+
+    @offset
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+
+    @offset
+    M=M+1
+@BLACK
+0;JMP
+
+(WHITE)
+    @offset
+    D=M
+    @8191
+    D=D-A //n-R1
+    @LOOP_KBD
+    D;JGT //n-R1<0 continue in loop else jump to end
+
+    @offset
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+
+    @offset
+    M=M+1
+@WHITE
+0;JMP
